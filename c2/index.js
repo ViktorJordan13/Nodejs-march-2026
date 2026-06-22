2c5f01332062bfe0f1b98897bbb5c9121e52514f
Viktor Jordanoski <viktorj560@gmail.com>
2026-06-22 22:02:19 +0200

    feat: c2 end

--- c2/index.js
+++ c2/index.js
@@ -120,3 +120,181 @@ const sortData = arrNums.sort((a, b) => {
 //Filter - ni vraka niza filtrirana spored usloot sto sme go zadale
 const filterData = arrNums.filter((el) => el > 5);
 // console.log(filterData);
+
+// Objects
+const student = {
+    grades: [7, 8, 6, 5, 10, 9, 10, 10],
+    name: "Gjuro",
+    age: 20,
+    university: {
+        name: "UKIM",
+        faculty: "FINKI",
+        subject: "Computer Science"
+    },
+    getAverage: () => {
+        let sum = 0;
+        for(let grade of student.grades){
+            sum += grade;
+        }
+        return sum/student.grades.length;
+    }
+};
+
+// console.log(student.university.faculty);
+// console.log(student.getAverage());
+
+function isEven(number){ //funkcija sto proveruva za parni broevi
+    return number % 2 === 0;
+}
+
+function isOdd(number){ // funkcija sto proveruva za neparni broevi
+    return number % 2 != 0;
+}
+
+// console.log(isEven(8));
+// console.log(isOdd(3));
+
+//Homework
+
+const studenti = [
+    { ime: "Bojan", prosek: 7.5, grad: "Skopje" }, //[0] => {}
+    { ime: "Pero", prosek: 8.3, grad: "Bitola" },
+    { ime: "Janko", prosek: 6.9, grad: "Bitola" },
+    { ime: "Vesna", prosek: 9.2, grad: "Skopje" },
+    { ime: "Elena", prosek: 9.9, grad: "Kumanovo" },
+    { ime: "Vancho", prosek: 10, grad: "Tetovo" },
+    { ime: "Elena", prosek: 9.9, grad: "Ohrid" },
+    { ime: "Ivana", prosek: 6.9, grad: "Kumanovo" },
+    { ime: "Natasha", prosek: 8.1, grad: "Skopje" },
+    { ime: "Stanko", prosek: 7.2, grad: "Strumica" },
+];
+
+//Zadaci:
+//1. Site studenti od Skopje cie ime zavrsuva na bukvata a i imaat prosek nad 7, podredeni po ime(rastecki-ascending)
+//2. Site studenti koi imaat prosek nad 9, ne se od Skopje, podredeni po prosek, no opagacki (descending)
+//3. Prvite 3 studenti koi immat iminja od 5 karakteri(bukvi), podredeni po prosek.
+//4. Gradovite podredeni spored grupnata visina na prosekot na studentite od niv
+//5. VKupen prosek na studenti ciesto ime zavrsuva na bukvata a, i posle toa isto taka na site ostanati
+
+// ----------------------------------------------------------------------------------------------------------------------------
+
+// Callback function
+
+// Primer
+
+//obicna funkcija sto ispisuva nesto
+
+const callMeBack = () => {
+    console.log("Kartikata vi e gotova, dojdete da ja zemete");
+}
+
+//callback funkcija
+const callbackFunkcija = (fn) => {
+    fn();
+}
+
+//callbackFunkcija(callMeBack);
+
+// Javascript e single threaded jazik, se izvsuva linija po lija kako sto se cita od gore na dolu.
+// Toa znaci deka ne moze paralelno da izvrsuva drug proces
+
+// Syncrhonous(blokiracki) - koga ke dojdes do toj del od kodot, ne prodolzuvaj natamu dodeka ne go izvrsis === blocking
+// Asynchronous(ne blokiracki) - koga ke stignes do toj del od kodot, pocni da go izvrsuvas, no prodolzi so izvrsuvanjeto === non blocking
+
+// Verzii na javascript
+
+//ES5 - Callback
+
+// Callback hell - vgnezduvanje na poveke callbacks
+// Primer
+
+// function greetMe(callback){
+//    if(callback){
+//        fn1()
+//            fn2()
+//                fn3()
+//    }
+// }
+
+const { printMe } = require("./test");
+//printMe();
+
+//CRUD - Create, Read, Update, Delete
+
+const fs = require("fs") //file system - core module
+//const {Validator} = require("node-input-validator"); //third party module
+
+//ES6 - Promise
+
+// Promises
+
+//step 1 - prazna funkcija (funkcija so prazno telo)
+
+const fileWrite1 = () => {
+
+};
+
+// step 2 - Promise e objekt, kogo go povikuvame so nov keyword, ondnosno instancirame nov objekt (Promise)
+
+const fileWrite2 = () => {
+    return new Promise();
+}
+
+// step 3 - vnatre vo Promisot imame anonimna funkcija kako prv parametar sto vraka druga funkija
+
+const fileWrite3 = () => {
+    return new Promise(() => {});
+}
+
+// step 4 - sega stavame da vraka funkcii, ama ova ke bidat success i fail (parametri sto ke se callback funkcii)
+
+const fileWrite4 = () => {
+    return new Promise((success, fail) => {});
+}
+
+// step 5 - vnatre zapisuvame nekoj fajl
+
+const fileWrite5 = () => {
+    return new Promise((success, fail) => {
+        fs.writeFile("data.txt", "Node.js module0", (err) => {
+            if(err) console.log(err);
+            console.log("File was written succesfully");
+        })
+    });
+}
+
+// step 6 - namesto racno da pecatime vrednosti (hardcoded, gi povikuvame callback funkciite fail() i success())
+
+const fileWrite6 = () => {
+    return new Promise((success, fail) => {
+        fs.writeFile("data.txt", "Node.js module0", (err) => {
+            if(err){
+                return fail(err);
+            }else{
+                return success();
+            }
+        })
+    });
+};
+
+// step 7 - gi zamenuvame hardkodiranite vrednosti na imeto na fajlot i podatocite so filename i data, gi dodavame kako input parametri
+
+const fileWrite7 = (filename, data) => {
+    return new Promise((success, fail) => {
+        fs.writeFile(filename, data, (err) => {
+            if(err){
+                return fail(err);
+            }else{
+                return success();
+            }
+        })
+    });
+};
+
+fileWrite7("data.txt", "Node.js course");
+fileWrite7("novFajl.txt", "Casot za denes zavrsi, dovolno e!")
+
+// Promise responses can be:
+//1. Fulfilled (positive)
+//2. Rejected (negative)
+//3. Pending (in progress) after some timeout it is negative, rejected
