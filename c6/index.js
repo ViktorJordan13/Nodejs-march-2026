411a7619bfb000cd248cb25085609eec28275a6c
Viktor Jordanoski <viktorj560@gmail.com>
2026-07-06 21:56:30 +0200

    feat: c6 end

--- c6/index.js
+++ c6/index.js
@@ -73,3 +73,111 @@ server.listen(8080);
 // 3. Each of them should be on a different route.
 // 4. Display it when you run the server
 // Optional: set headers for each of them
+
+//localhost:xxxx/sobiranje/2/2
+const handlerCalculator = (req, res) => {
+    const url = req.url;
+
+    // if (url === "/"){
+    //     res.write("home");
+    // }
+    // res.end("OK");
+
+    const [_, operacija, num1, num2] = req.url.split("/");
+
+    const stringNumber = "1";
+    Number(stringNumber); // converts string to number
+
+    const number = 1;
+    number.toString(); // converts number into string
+
+    let result;
+
+    //"2" + "2" = not ok
+    // 2 + 2 = ok
+
+    switch(operacija){
+        case "sobiranje":
+            result = Number(num1) + Number(num2);
+            res.end(`${result}`);
+            break;
+        case "odzemanje":
+            result = Number(num1) - Number(num2);
+            res.end(`${result}`);
+            break;
+        case "mnozenje":
+            result = Number(num1) * Number(num2);
+            res.end(`${result}`);
+            break;
+        case "delenje":
+            result = Number(num1) / Number(num2);
+            res.end(`${result}`);
+            break;
+        default:
+            res.end("Nepoznata operacija");
+    }
+}
+
+const newServer = http.createServer(handlerCalculator);
+
+newServer.listen(8000);
+
+// Homework
+// 1. kreirajte server
+// 2. na ruta /home/imePrezimePol -> res.end(<imetoPrezimetoPolot), prvata bukva na sekoe da bide golema
+// 3. na ruta /home/title -> dodadete naslov na rutata
+// 4. na ruta /home/header -> dodadete vrednost "Ova e header!"
+// Optional na handler funkcijata za kalkulator pogore od ovoj cas dodadete modul (%), kvadrat (n^2) i kub (n^3)
+
+//------------------------------------------------------------------------------------------------------------------------
+
+// Middleware - set of functions or code snippets that are executed in a sequnatial manner during the processing of HTTP request or response
+
+// Design pattern - general repeatable solution to a commonly occuring problem in software desing
+
+// MVC (Model View Controller) - it is used to separate different functionalities and responsibilites within an application
+
+// 1. Model -> the model represents the data (students, cars, movies, crypto currencies, etc ...) and the bussiness logic
+// of the application (in our case simple CRUD functionalities)
+
+// 2. Controller -> the controller acts as an intermediary(bridge) between the model and the view
+
+// 3. View -> the view is resposible for presenting the data to the user (html)
+
+// Express module -> wrapper around http core module (that means that express is not a core module)
+
+// npm install express
+
+const express = require("express");
+
+const app = express();
+// Methods: GET, POST, PUT, PATCH, DELETE
+
+// Middleware to parse URL-encoded data
+app.use(express.urlencoded({ extended: false })); // POST, PUT, PATCH
+// in the above example, express.urlencoded({ extended: false }) is added as a middleware app.use()
+// The extended option determines whetehr to use the querying library (false) or use the qs library for parsing URL-encoded data
+// Once this middleware is added, we can access the parsed form in the req.body object within our routes
+// handler(contoller)
+// The req.body object will contain key-value pairs corresponding to the form fields.
+
+// THIS IS FOR EXPRESS THAT IS LOWER OR EQUAL TO 4.16!
+// If your express version is higher than 4.16, instead of using express.urlencoded middleware, you can install and use the body-parser
+// middleware to achieve the same functionality
+
+app.get("/", (req, res) =>{
+    res.send("My first server with express");
+});
+
+app.get("/form", (req, res) => {
+    // Access the form data from req.body
+    console.log(req.body);
+    res.send(" Form submitted succesfully");
+});
+
+//app.post("/form", (req, res) => ...
+
+app.listen(3000, (err) => {
+    if(err) console.log("Greska", err);
+    console.log("Server listening on port 3000!");
+});
