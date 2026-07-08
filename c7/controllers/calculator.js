18bac0bef7f0ca1286c3bed73146c2a3eaaee6d0
Viktor Jordanoski <viktorj560@gmail.com>
2026-07-08 20:22:12 +0200

    feat: c7 task

---
+++ c7/controllers/calculator.js
@@ -0,0 +1,76 @@
+const fs = require("fs");
+
+const getCalculator = async(req, res) => {
+    try{
+        let output = await parseTemplate("calculator_form");
+        res.send(output);
+
+    }catch(err){
+        console.log(err);
+        res.status(500).send("Internal server error");
+    }
+}
+
+const postCalculator = (req, res) => {
+    
+    const {op, a, b} = req.body;
+
+    if(a === "" || b === ""){
+        return res.status(400).send("Bad request");
+    }
+
+    let result;
+
+    switch(op){
+        case "sobiranje":
+            result = Number(a) + Number(b);
+            //res.end(`${result}`);
+            break;
+        case "odzemanje":
+            result = Number(a) - Number(b);
+            //res.end(`${result}`);
+            break;
+        case "mnozenje":
+            result = Number(a) * Number(b);
+            //res.end(`${result}`);
+            break;
+        case "delenje":
+            result = Number(a) / Number(b);
+            //res.end(`${result}`);
+            break;
+        default:
+            res.end("Nepoznata operacija");
+    }
+    try{
+        let output = await parseTemplate("calculator", {data: result});
+        res.send(output);
+    }catch(err){
+        console.log(err);
+        res.status(500).send("Internal server error");
+    }
+}
+
+const parseTemplate = async(template, data = null) => {
+    // ako ne ja povikame parseTemplate so vistinski podatoci, togas data ke ni stoi vo svojata default vrednost sto e null
+    console.log("path", `${__dirname}/../views/${template}.html`);
+    return new Promise((success, fail)=> {
+        fs.readFile(
+            `${__dirname}/../views/${template}.html`, // so ova sakame da dojdeme do calculator_form.html i calculator.html i da gi procitame
+            "utf-8",
+            (err, content) => {
+                if(err) return fail(err);
+                if(data){
+                    for(d in data){
+                        content = content.replaceAll(`{{${d}}}`, data[d]);
+                    }
+                }
+                return success(content);
+            }
+        );
+    });
+};
+
+module.exports={
+    getCalculator,
+    postCalculator
+}
---
+++ c7/index.js
@@ -0,0 +1,6 @@
+//Task
+// So express modulot:
+// 1. kreirajte nov server
+// 2. na home ispisete "Hello students!"
+// 3. na /hello ispisete "Hello world"
+// OPTIONAL na adressa "/hello/:name" ispisete na mestoto na (:name) vaseto ime i ispisete go na browserot 
