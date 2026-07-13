cb57ba5f36054d8835c8e822b8c428f6bdf71f2f
Viktor Jordanoski <viktorj560@gmail.com>
2026-07-13 20:29:25 +0200

    feat: c8 break

---
+++ c8/index.js
@@ -0,0 +1,7 @@
+//MERN stack = Mongo(NoSQL), Express, React, Node
+
+//Task
+//1. Install axios or any simmilar module to fetch data from https://jsonplaceholder.typicode.com/users
+//2. Display the data from that API - usernames 
+//3. Display the data from that API - their email and website
+//html and css
